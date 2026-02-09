29ed5974218a50f0fe4d47f6bf918baf2369612a
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2026-02-09 08:31:00 +0530

    Clang format

--- src/initial_configs.h
+++ src/initial_configs.h
@@ -3,8 +3,9 @@
 #include <utility>
 #include <vector>
 namespace offsets {
-	const int glider[5][2] = {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 1}};
-	const int f_pentonimo[5][2] = {{0, 0}, {0, 1}, {1, -1}, {1, 0}, {2, 0}};
-}
+const int glider[5][2] = {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 1}};
+const int f_pentonimo[5][2] = {{0, 0}, {0, 1}, {1, -1}, {1, 0}, {2, 0}};
+} // namespace offsets
 
-std::vector<std::pair<int, int>> applyOffsets(int* offsets, int N, int initial_i = 0, int initial_j = 0);
+std::vector<std::pair<int, int>>
+applyOffsets(int *offsets, int N, int initial_i = 0, int initial_j = 0);
